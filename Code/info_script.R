1366c3b644197fb590c6c4a5fdda756005f4c90d
Erin Johnston <erin.johnstonno.4@gmail.com>
2021-11-19 10:39:36 -0800

    Add Updated File
    Brining all code together into one file to share with collaborators

--- Code/info_script.R
+++ Code/info_script.R
@@ -1,2 +1,56 @@
 
-## Things in this folder relate to code for 2021 CCFRP Angler Survey
+## Things in this script relate to code for 2021 CCFRP Angler Survey
+
+
+## Detailed changes to the raw data file
+
+######## changes to Q1 - What year did you first volunteer with CCFRP? ######
+## We should not have left this as an open ended question... it has left room for interesting answers
+## If we would like to use length of time volunteered in future analyses, changed to the data have to be made at the raw data level. See below for a complete list of changes
+
+## I don't remember, maybe 5 years ago --> 2016
+## 2018(?) --> 2018
+## 2012 ( I think) --> 2012
+## since the second year --> 2008 (because this person's primary institution was MLML)
+## August 14,2013 --> 2013
+## Facebook --> DELETED
+## 2007 out of Moss Landing, or whatever was the first year for CCFRP --> 2007
+## Don't know, maybe 2014 --> 2014
+## 2008?? --> 2008
+## 2016 ? --> 2016
+## The  first year it came out. I do not know the exaact timeframe --> 2017 (because this person's primary institution was SIO)
+## 2007 or 2008 --> 2007
+## The first year --> 2007 (because this person's primary institution was Cal Poly SLO)
+## Since the beginning --> 2017 (because this person's primary institution was SIO)
+## 2019? --> 2019
+## about 2018??? --> 2018
+## March 2013 with Lyall and Devon --> 2013
+## Not sure...however, I was involved from "day 1"...the very first year --> 2017 (because this person's primary institution was SIO)
+## 2006 or 2007 --> 2007
+## 2006-2007 or there about --> 2007
+## First year the program started in the central coast. --> 2007
+## ? --> DELETED
+## Whenevet it started --> 2017 (because this person's primary institution was HSU)
+## 2018 (I think) --> 2018
+## The first year of the program 2015 or2016. --> 2017 (because this person's primary institution was SIO)
+## Approximately 8 years --> 2013
+## Canâ€™t remember --> DELETED
+## the first year --> 2007 (because this person's primary institution was Cal Poly SLO)
+## 20118 --> 2018 (Could have been 2011 or 2018, but the person's primary institution was SIO)
+## 2009? --> 2009
+## 2017? --> 2017
+## not sure, estimated 8 yrs --> 2013
+## I havenâ€™t been out yet but very interested --> DELETED
+## Not sure of the year, however, it was the FIRST trip out of San Diego on the Outer Limits --> 2017
+
+
+
+##### changes to q39 (question) Based on the above graphic how interconnected are you with nature? Choose a number between 1-7 ######
+## This was another question that was left open-ended
+
+## 4+ --> 4
+## I selected 4, it shows a better balance of the spheres with Mother Earth --> 4
+## 7.  I have fished, dived, hiked, and camped, for my whole life and believe in the interconnectness of all things. --> 7
+## 4/5/2021 (excel read this as a date, but the person put 4-5) --> 4
+
+
